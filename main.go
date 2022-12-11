745beeab5186a7d53eadd4ef367b976257afdcad
Julian Arz <j.arz@web.de>
2022-12-12 00:17:01 +0100

    wasm exports a function with string argument

--- main.go
+++ main.go
@@ -6,23 +6,28 @@ import (
 	"os"
 )
 
-//go:export check
-func check(num int64) bool {
+//go:export checkstr
+func checkstr(num string) string {
+	number := new(big.Int)
+	_, err := fmt.Sscan(os.Args[1], number)
+	if err != nil {
+		return "error scanning value: " + err.Error()
+	} else {
+		isPrime := number.ProbablyPrime(10)
+		if isPrime {
+			return num + " is probably prime\n"
+		} else {
+			return num + " is not prime\n"
+		}
+	}
+}
+
+//go:export checknum
+func checknum(num int64) bool {
 	number := big.NewInt(num)
 	return number.ProbablyPrime(10)
 }
 
 func main() {
-	number := new(big.Int)
-	_, err := fmt.Sscan(os.Args[1], number)
-	if err != nil {
-		fmt.Printf("error scanning value: %s\n", err.Error())
-	} else {
-		isPrime := number.ProbablyPrime(10)
-		if isPrime {
-			fmt.Printf("%s is probably prime\n", os.Args[1])
-		} else {
-			fmt.Printf("%s is not prime\n", os.Args[1])
-		}
-	}
+	print(checkstr(os.Args[1]))
 }
