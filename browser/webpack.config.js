0d93cb26a83f30be2b6261b7da9d57dc021985e6
Julian Arz <j.arz@web.de>
2022-12-16 21:20:30 +0100

    configures prettier to use 4 spaces

--- browser/webpack.config.js
+++ browser/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  mode: "development",
-  entry: "./src/prime.js",
-  externals: {
-    "wasmer_wasi_js_bg.wasm": true,
-  },
-};
+    mode: 'development',
+    entry: './src/prime.js',
+    externals: {
+        'wasmer_wasi_js_bg.wasm': true,
+    },
+}
