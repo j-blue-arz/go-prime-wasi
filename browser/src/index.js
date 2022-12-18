37d865419d72a8400f3cc80219e93fe2ee572bce
Julian Arz <j.arz@web.de>
2022-12-18 21:14:49 +0100

    finishes browser implementation

--- browser/src/index.js
+++ browser/src/index.js
@@ -1,8 +1,14 @@
-import { computePrime } from "./prime.js"
+import { computePrime } from './prime.js'
 
+const numberInput = document.getElementById('number')
 const computeButton = document.getElementById('compute')
-computeButton.onclick = () => isPrime(7)
+const outputDiv = document.getElementById('output')
+computeButton.onclick = () => onClickCompute()
 
-async function isPrime(number) {
-    await computePrime()
+async function onClickCompute() {
+    if (numberInput.reportValidity()) {
+        const value = numberInput.value
+        const result = await computePrime(value)
+        outputDiv.textContent = result
+    }
 }
--- browser/src/prime.js
+++ browser/src/prime.js
@@ -1,25 +1,21 @@
 import { init, WASI } from '@wasmer/wasi'
 
-export async function computePrime() {
-    await init()
+let module = undefined;
+
+export async function computePrime(number) {
+    if (module === undefined) {
+        await init();
+        module = await WebAssembly.compileStreaming(fetch('prime.wasm'));
+    }
 
     let wasi = new WASI({
         env: {},
         args: [
-            'prime.wasm', '854654651354613023354657879465120356462134'
+            'prime.wasm', number
         ],
-    })
-
-    const moduleBytes = fetch('prime.wasm')
-    const module = await WebAssembly.compileStreaming(moduleBytes)
-    // Instantiate the WASI module
-    await wasi.instantiate(module, {})
-
-    // Run the start function
+    });
     
-    let exitCode = wasi.start();
-    let stdout = wasi.getStdoutString()
-
-    // This should print "hello world (exit code: 0)"
-    console.log(`${stdout}(exit code: ${exitCode})`)
+    await wasi.instantiate(module, {});
+    wasi.start();
+    return wasi.getStdoutString();
 }
